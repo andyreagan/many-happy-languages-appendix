977d21ee0813cfa589eef21d32ed6269663fcb48
Andy Reagan <github@andyreagan.com>
2014-06-10 15:49:27 -0400

    initial commit.

---
+++ js/minify.sh
@@ -0,0 +1,8 @@
+# wget https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+
+\rm books-all{,.min}.js
+cat jquery.min.js bootstrap.min.js d3.andy.js urllib.js plotShift.js shift.js drawLens-chapters.js selectChapter.js selectChapterTop.js drawBookTimeseries.js computeHappsChapters.js books-on-load.js topteam.js >> books-all.js
+
+\rm translations-all{,.min}.js
+cat jsapi jquery.min.js bootstrap.min.js d3.andy.js urllib.js translations.js topteam.js >> translations-all.js
+
